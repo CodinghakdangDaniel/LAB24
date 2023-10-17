95fc4022d7e10585c782865bf50440184a944d02
CodinghakdangDaniel <junh2e97@gmail.com>
2023-10-18 04:23:53 +0900

    lab24

--- script.js
+++ script.js
@@ -4,23 +4,64 @@ const btn = document.getElementById('btn');
 
 
 //입력 값 대조
-const sampleValue = document.getElementById('sample');
-const inputValue = document.getElementById('text-area');
+const sample = document.getElementById('sample');
+const inputBox = document.getElementById('text-area');
 
-console.log(sampleValue);
-console.log(inputValue.value);
+// sample.style.color = 'red'
+const sampleValue = document.getElementById('sample').innerHTML;
+// const inputValue = document.getElementById('text-area').value;
 
-function compareValue() {
-    if(sampleValue.value === inputValue.value) {
-        sampleValue.style.color = green;
-    }
-    else {
-        sampleValue.style.color = red;
-        console.log(sampleValue.style.color)
-    }
-};
+const sampleValueSplit = sampleValue.split("");
+// const inputValueSplit = inputValue.split("");
 
-compareValue();
+// console.log(sampleValueSplit);
+// console.log(inputValue); // -> 빈 배열 
+
+//스트링을 array로  -> 일일이 비교 for each 나 map 사용 -> 한번 수정하면 다시 처음으로 가서 비교하기 때문에 loop이 필요함.
+
+
+inputBox.addEventListener('input', () => {
+    sampleValueSplit.forEach((item, index) => {
+        // console.log(item, index);
+        console.log(sampleValueSplit)
+
+        // 1안 
+        // let x  = inputBox.value;
+        // console.log(x);
+        // console.log(sampleValue)
+        // if(x === sampleValue) {
+        //     sample.style.color = 'green'
+        // }
+        //
+
+        // 2안
+        let x  = inputBox.value;
+        let newArr = x.split("");
+        console.log(newArr);
+        if(newArr.toString() === sampleValueSplit.toString()) {
+            sample.style.color = 'green'
+        }
+        //
+
+
+        // 3안
+        // console.log(newArr);
+        // console.log(newArr[0])
+        // console.log(newArr[1])
+        // if(newArr[0] === sampleValueSplit[0]) {
+        //     sampleValueSplit[0].style.color = 'green'
+        // };
+        
+    });
+
+})
+
+
+
+
+
+
+
 
 
 
@@ -50,4 +91,4 @@ function setTimer() {
     // console.log(playTime)
 };
 
-btn.addEventListener('click', setTimer());
+btn.addEventListener('click', () => setTimer()); // 익명함수안에 필요한 함수
